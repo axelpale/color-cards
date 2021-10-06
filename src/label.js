a324c4245eac099760cf09708d6cdcf1031a34c3
Akseli Palen <akseli.palen@gmail.com>
2021-10-06 13:58:32 +0300

    round instead of floor for correct values

--- src/label.js
+++ src/label.js
@@ -1,34 +1,34 @@
 exports.css = (rgb) => {
   return 'rgb(' +
-    Math.floor(rgb.r) + ',' +
-    Math.floor(rgb.g) + ',' +
-    Math.floor(rgb.b) + ')'
+    Math.round(rgb.r) + ',' +
+    Math.round(rgb.g) + ',' +
+    Math.round(rgb.b) + ')'
 }
 
 exports.hex = (rgb) => {
   return 'HEX ' +
-    Math.floor(rgb.r).toString(16) + ' ' +
-    Math.floor(rgb.g).toString(16) + ' ' +
-    Math.floor(rgb.b).toString(16)
+    Math.round(rgb.r).toString(16) + ' ' +
+    Math.round(rgb.g).toString(16) + ' ' +
+    Math.round(rgb.b).toString(16)
 }
 
 exports.rgb = (rgb) => {
   return 'RGB ' +
-    Math.floor(rgb.r) + ' ' +
-    Math.floor(rgb.g) + ' ' +
-    Math.floor(rgb.b)
+    Math.round(rgb.r) + ' ' +
+    Math.round(rgb.g) + ' ' +
+    Math.round(rgb.b)
 }
 
 exports.cielab = (lab) => {
   return 'LAB ' +
-    Math.floor(lab.l) + ' ' +
-    Math.floor(lab.a) + ' ' +
-    Math.floor(lab.b)
+    Math.round(lab.l) + ' ' +
+    Math.round(lab.a) + ' ' +
+    Math.round(lab.b)
 }
 
 exports.cielch = (lch) => {
   return 'LCH ' +
-    Math.floor(lch.l) + ' ' +
-    Math.floor(lch.c) + ' ' +
-    Math.floor(lch.h)
+    Math.round(lch.l) + ' ' +
+    Math.round(lch.c) + ' ' +
+    Math.round(lch.h)
 }
